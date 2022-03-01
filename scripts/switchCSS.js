840c1e0e9727a744d0b5e3e3d062fa3a244654d8
hotvott <56598606+hotvott@users.noreply.github.com>
2022-03-01 19:06:24 +0100

    Add files via upload

---
+++ scripts/switchCSS.js
@@ -0,0 +1,7 @@
+/* Quick hack for switching a CSS file by mouse click. (C) DJ 8/2012 */
+function switchCSS(first, second) {
+var styleLink = document.getElementById("replaceLink");
+var href = styleLink.getAttribute("href");
+newHref = href == first ? second : first;
+styleLink.setAttribute("href", newHref);
+}
